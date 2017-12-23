59fcf3a99b2ee8a91dfeadbc97d422ee01a6e44f
github username <github email address>
2017-12-23 17:05:42 -0500

    added phone to restaurant class

--- veg_out/lib/restaurant.rb
+++ veg_out/lib/restaurant.rb
@@ -1,11 +1,12 @@
 
 class Restaurant
-  attr_accessor :name, :distance, :address, :description
+  attr_accessor :name, :distance, :address, :phone, :description
 
   def initialize(name, distance, address)
     @name = name
     @distance = distance
     @address = address
+    @phone = phone
   end
 
 
