1a0c96af2f5fccaabecc458c48032f0813340991
github username <github email address>
2017-12-23 16:20:45 -0500

    edited welcome message

--- veg_out/lib/cli.rb
+++ veg_out/lib/cli.rb
@@ -2,8 +2,8 @@ class VegOut::CLI
 
   def call
     puts "Welcome to Veg Out!!"
-    puts "Your One Stop Shop to find nearby vegan and vegetarian restaurants!!"
+    puts "The best place to find nearby vegan and vegetarian restaurants!!"
   end
 
-  
+
 end
