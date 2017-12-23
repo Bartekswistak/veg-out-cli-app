a2b225649b7a0383044c76a019bce9f8609c3f43
github username <github email address>
2017-12-23 15:20:23 -0500

    updated initialize method in restaurant class

--- veg_out/lib/restaurant.rb
+++ veg_out/lib/restaurant.rb
@@ -1,6 +1,12 @@
+
 class Restaurant
   attr_accessor :name, :distance, :address, :description
 
-  
+  def initialize(name, distance, address)
+    @name = name
+    @distance = distance
+    @address = address
+  end
+
 
 end
--- veg_out/lib/scraper.rb
+++ veg_out/lib/scraper.rb
@@ -2,13 +2,17 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
+require_relative './restaurant.rb'
+
 
 
 class Scraper
   html = open("https://www.happycow.net/searchmap?lat=&lng=&location=34293")
   doc = Nokogiri::HTML(html)
 
-  binding.pry
+
+binding.pry  
+
 
 
 
