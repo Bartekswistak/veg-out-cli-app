a37219e5cbd52d5df67caec833f090928ad70d50
github username <github email address>
2017-12-29 16:35:07 -0500

    worked on cli

--- veg_out/lib/cli.rb
+++ veg_out/lib/cli.rb
@@ -15,7 +15,8 @@ class VegOut::CLI
   end
 
   def start
-    puts "Please enter your 5 digit zip code:"
+    puts ""
+    puts "Please enter the name of your city, or a 5 digit zip code:"
     input = gets.strip.to_s
     url = "https://www.happycow.net/searchmap?lat=&lng=&location=#{input}&vegan=true&vegetarian=true&vegfriendly=true&distance=15&distanceType=mi&limit=18"
     html = open(url)
@@ -25,22 +26,29 @@ class VegOut::CLI
 
   def show_restaurants
       @restaurants = @doc.css("h5").map {|name| name.text}.uniq
-        @restaurants.each_with_index.map {|n, i| puts "#{i + 1}. #{n}"}
+        @restaurants.each_with_index.map {|n, index| puts "#{index + 1}. #{n}"}
+      more_info
+    end
 
-      puts "For more information on a restaurant, type locations to show addresses or type restart to start over:"
-        input = gets.strip
-        case input
-        when "restart"
-           start
-         when "locations"
-           show_addresses
-        end
 
+  def more_info
+        puts ""
+        puts "For more information, enter locations, restaurants, start over or exit to quit"
+          input = gets.strip
+          case input
+          when "start over"
+             start
+           when "locations"
+             show_addresses
+           when "restaurants"
+             show_restaurants
+          end
   end
 
   def show_addresses
     address = @doc.css("div.details").children.css("p").map {|location| location.text}
     address.each_with_index.map {|n, i| puts "#{i + 1}. #{n}"}
+    more_info
   end
 
 
