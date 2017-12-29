40f880356166b51083f9f2171e7487670ef5a7a2
github username <github email address>
2017-12-29 15:45:04 -0500

    CLI will return list of nearby restaurants

--- veg_out/lib/cli.rb
+++ veg_out/lib/cli.rb
@@ -1,12 +1,31 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
 class VegOut::CLI
 
   def call
     puts ""
     puts "                    Welcome to Veg Out   "
     puts "                    ------------------   "
-    puts "The best place to find vegan and vegetarian restaurants in your area!!"
+    puts "The best place to find vegan and vegetarian restaurants!!"
     puts ""
+    start
   end
 
+  def start
+    puts "Please enter your 5 digit zip code:"
+    input = gets.strip.to_s
+    url = "https://www.happycow.net/searchmap?lat=&lng=&location=#{input}&vegan=true&vegetarian=true&vegfriendly=true&distance=15&distanceType=mi&limit=18"
+    html = open(url)
+    @doc = Nokogiri::HTML(html)
+    show_restaurants
+  end
 
+  def show_restaurants
+      @restaurants = @doc.css("h5").map {|name| name.text}.uniq
+        @restaurants.each_with_index.map {|n, i| puts "#{i + 1}. #{n}"}
+      @restaurants
+  end
 end
--- veg_out/lib/scraper.rb
+++ veg_out/lib/scraper.rb
@@ -6,12 +6,13 @@ require_relative './restaurant.rb'
 
 class Scraper
 
+  def scrape
     puts "Please enter your 5 digit zip code:"
     input = gets.strip.to_s
     url = "https://www.happycow.net/searchmap?lat=&lng=&location=#{input}&vegan=true&vegetarian=true&vegfriendly=true&distance=15&distanceType=mi&limit=18"
     html = open(url)
     doc = Nokogiri::HTML(html)
-
+end
     restaurants = doc.css("h5").map {|name| name.text}.uniq
     distance = doc.css("span.distance").map {|howfar| howfar.text}.uniq
     address = doc.css("div.details").children.css("p").map {|location| location.text}
