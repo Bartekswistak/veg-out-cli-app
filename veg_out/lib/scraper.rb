79283ef47dc58feb5d9fbddeae21caa3a5f87bc3
github username <github email address>
2017-12-30 13:53:37 -0500

    used pry to scrape second level attributes

--- veg_out/lib/scraper.rb
+++ veg_out/lib/scraper.rb
@@ -6,15 +6,10 @@ require_relative './restaurant.rb'
 
 class Scraper
 
-    puts "Please enter your 5 digit zip code:"
     input = gets.strip.to_s
-    url = "https://www.happycow.net/searchmap?lat=&lng=&location=#{input}&vegan=true&vegetarian=true&vegfriendly=true&distance=15&distanceType=mi&limit=18"
+    url = "https://www.happycow.net/reviews/mango-bistro-englewood-36923"
     html = open(url)
     doc = Nokogiri::HTML(html)
-
-    restaurants = doc.css("h5").map {|name| name.text}.uniq
-    distance = doc.css("span.distance").map {|howfar| howfar.text}.uniq
-    address = doc.css("div.details").children.css("p").map {|location| location.text}
 
 
 binding.pry
