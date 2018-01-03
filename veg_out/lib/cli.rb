a40f3b03365584b4acdfb38c09b29725ca9780d8
github username <github email address>
2018-01-03 16:38:53 -0500

    removed not need methods from cli class

--- veg_out/lib/cli.rb
+++ veg_out/lib/cli.rb
@@ -48,10 +48,6 @@ class VegOut::CLI
         case input
           when "back"
              start
-  #        when "locations"
-  #           show_addresses
-  #        when "restaurants"
-  #           show_restaurants
           when "quit"
              exit
           when "deets"
@@ -63,46 +59,9 @@ class VegOut::CLI
           end
     end
 
-#   def show_addresses
-#    address = @doc.css("div.details").children.css("p").map {|location| location.text}
-#      address.each_with_index.map {|n, i| puts "#{i + 1}. #{n}"}
-#    more_info
-# end
-
-  def scrape_details_page
-#    @details = @doc.css("div.thumbnail__box a").map {|link| link['href']}.uniq
-    # list_urls = details.each_with_index.map {|n, i| "#{i + 1}. #{n}"}
-#    @base_url = "https://www.happycow.net"
 
     # The idea here is take user input, and take the appropriate link from details
     # and add it to the base site variable, from there more details will be scraped and displayed.
-
-    puts ""
-    puts "Enter the number of the restaurant you would like more info on:"
-    puts "Or to go back, type back or type quit to exit"
-    puts ""
-
-    input = gets.strip
-
-
-    case input.to_i
-    when 1..26        #method not working properly yet
-          puts ""
-          show_details
-          puts ""
-#      when "back"
-#          show_restaurants
-#      when "quit"
-#          exit
-#    when "show"
-#      show_details
-      else
-          puts ""
-          puts "Invalid choice!"
-          puts ""
-      end
-      scrape_details_page
-end
 
     def show_details    # not working yet
       puts "Which restaurant are you interested in learing more about:"
