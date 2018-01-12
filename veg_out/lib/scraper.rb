7ce69f0ea02da46e10a35db3f12f360c4be60381
github username <github email address>
2018-01-11 20:41:52 -0500

    removed unnecessary instance variables

--- veg_out/lib/scraper.rb
+++ veg_out/lib/scraper.rb
@@ -35,14 +35,14 @@ class VegOut::Scraper
   end
 
   def self.find_details_page
-    @info = gets.strip.to_i
-      if @info > @restaurants.size
+    info = gets.strip.to_i
+      if info > @restaurants.size
         puts "Invalid choice. Select a number from the list above:"
         self.find_details_page
       else
         details = @doc.css("div.thumbnail__box a").map {|link| link['href']}.uniq
         base_url = "https://www.happycow.net"
-        site = base_url << details[@info-1].to_s
+        site = base_url << details[info-1].to_s
         @deets = Nokogiri::HTML(open(site))
         self.more_details
       end
